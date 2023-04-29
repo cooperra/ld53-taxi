d946ab68bca099c74fc11fdff66d6acd77f817ca
Edward Curtis <gamerguy1835@yahoo.com>
2023-04-28 23:58:08 -0400

    Player Movement Now Has Rudimentary Player Vertical Boundaries

--- Assets/Game/Scripts/PlayerMovement/PlayerCarMovement.cs
+++ Assets/Game/Scripts/PlayerMovement/PlayerCarMovement.cs
@@ -6,25 +6,48 @@ public class PlayerCarMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     [SerializeField] private float speed;
+    //Set this value to 4.5
+    private float boundary = 4.5f;
 
     #region PlayerInput plus speed;
     private float Forward()
     {
+         
+        
         return Input.GetAxis("Vertical") * speed;
     }
     private float Right()
     {
+        
         return Input.GetAxis("Horizontal") * speed;
     }
     #endregion
     //Player Car Movement;
     Vector3 Movement()
     {
-        Vector3 changeInPos = ( Vector3.up * Forward()) + (Vector3.right * Right());
-
+        Vector3 fwd = Vector3.up * Forward();
+        //Simple Collision Detection For Boundary. Fix if possible
+        if(transform.position.y >= boundary * 0.05f)
+        {
+            fwd.y = 0;
+            fwd.y-= speed * Time.deltaTime;
+        }
+        else if (transform.position.y <= -boundary)
+        {
+            fwd.y = 0;
+            fwd.y += speed * Time.deltaTime;
+        }
+        Vector3 changeInPos = (fwd + (Vector3.right * Right()));
+        
         return changeInPos;
     }
-
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        /*Handle Collisions for Obstacle
+         * Car Tag and Obstacle are the same label Obstacle
+         * 
+         */
+    }
     void MoveCar()
     {
         rb.MovePosition(transform.position + Movement() * Time.fixedDeltaTime);
