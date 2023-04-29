62d134b889bf6d05ae108781b68e7aecc7bd5089
Edward Curtis <gamerguy1835@yahoo.com>
2023-04-29 13:55:35 -0400

    Added Game Manager Class with specifications. Could we use Events system?

--- Assets/Game/Scripts/Game/GameManager.cs
+++ Assets/Game/Scripts/Game/GameManager.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+public enum Conditions
+{
+    win,
+    lose,
+    start,
+    exit
+}
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+ 
+    /*
+     * This manages start, win, conditions
+     * Handles Score that is read by playerMovement and UI Money
+     * At the end of the game the player explodes.
+     * exit the game exits
+     * doesn't do anything until the player starts
+     * 
+     */
+    //Trying to add events maybe?
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
